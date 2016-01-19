9c733e6f7bed67b756630cf6a343817bae06f9d9
mg180 <mattgordon180@gmail.com>
2016-01-19 00:17:54 +0000

    Initial commit
    RRA Hardware Controller

---
+++ Hardware/rra.vhd
@@ -0,0 +1,155 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+entity rra is
+port (
+		clk			: in  std_ulogic;
+		rst			: in  std_ulogic;
+
+		--Servo Positions
+		l1,l2		: out std_ulogic;
+		m1,m2		: out std_ulogic;
+		u1,u2		: out std_ulogic;
+
+		--Control
+		low_u		: in std_ulogic;
+		low_d		: in std_ulogic;
+		mid_u		: in std_ulogic;
+		mid_d		: in std_ulogic;
+		upp_u		: in std_ulogic;
+		upp_d		: in std_ulogic;
+		store 		: in std_ulogic;
+		speed		: in std_ulogic_vector(3 downto 0);
+		mode		: in std_ulogic_vector(1 downto 0)
+	 );
+end rra;
+
+architecture v1 of rra is
+
+	component memory
+	port(
+			clk		: in  std_ulogic;
+			rst 	: in  std_ulogic;
+
+			addr	: in  std_ulogic_vector(7 downto 0);
+			data_in	: out std_ulogic_vector(15 downto 0);
+			data_out: in  std_ulogic_vector(15 downto 0);
+			l_memory: in  std_ulogic;
+			w_memory: in  std_ulogic
+		);
+	end component;
+
+	component controller
+	port(
+			clk       : in  std_ulogic;
+	        rst       : in  std_ulogic;
+	        
+	        mode      : in  std_ulogic_vector(1 downto 0);
+	        moving    : in  std_ulogic;
+	        store     : in  std_ulogic;
+	        l_keypad  : out std_ulogic;
+	        l_memory  : out std_ulogic;
+	        w_memory  : out std_ulogic
+		);
+	end component;
+
+	component servo_controller
+	port(
+			clk		: in  std_ulogic;
+			rst		: in  std_ulogic;
+
+			speed 	: in  std_ulogic_vector(3 downto 0);
+			current	: out unsigned(15 downto 0);
+			target  : in  unsigned(15 downto 0);
+			pwm_out : out std_ulogic
+		);
+	end component;
+
+	--Memory
+	signal addr		: std_ulogic_vector(7 downto 0);
+	signal data_in	: std_ulogic_vector(15 downto 0);
+	signal data_out	: std_ulogic_vector(15 downto 0);
+	signal l_memory : std_ulogic;
+	signal w_memory : std_ulogic;
+
+	--Movement (c - current, t - target)
+	signal moving						: std_ulogic;
+	signal c_lower_pos,		t_lower_pos	: unsigned(15 downto 0);
+	signal c_middle_pos,	t_middle_pos: unsigned(15 downto 0);
+	signal c_upper_pos,		t_upper_pos	: unsigned(15 downto 0);
+
+	--Servo control
+	signal l_keypad 					: std_ulogic;
+	signal l_pwm, m_pwm, u_pwm			: std_ulogic;
+
+begin
+	
+	rra_memory	: memory port map(
+		clk 		=> clk,
+		rst 		=> rst,
+		addr		=> addr,
+		data_in		=> data_in,
+		data_out	=> data_out,
+		l_memory	=> l_memory,
+		w_memory	=> w_memory
+	);
+
+	rra_controller	: controller port map(
+		clk 		=> clk,
+		rst 		=> rst,
+		mode 		=> mode,
+		moving  	=> moving,
+		store 		=> store,
+		l_keypad	=> l_keypad,
+		l_memory	=> l_memory,
+		w_memory	=> w_memory
+	);
+
+	rra_servo_lower : servo_controller port map(
+		clk 		=> clk,
+		rst 		=> rst,
+		speed		=> speed,
+		current		=> c_lower_pos,
+		target		=> t_lower_pos,
+		pwm_out		=> l_pwm
+	);
+
+	rra_servo_middle: servo_controller port map(
+		clk 		=> clk,
+		rst 		=> rst,
+		speed		=> speed,
+		current		=> c_lower_pos,
+		target		=> t_lower_pos,
+		pwm_out		=> m_pwm
+	);
+
+	rra_servo_upper : servo_controller port map(
+		clk 		=> clk,
+		rst 		=> rst,
+		speed		=> speed,
+		current		=> c_lower_pos,
+		target		=> t_lower_pos,
+		pwm_out		=> u_pwm
+	);
+
+
+	movement 	: process(c_lower_pos, t_lower_pos, c_middle_pos, t_middle_pos, c_upper_pos, t_upper_pos)
+	begin
+		if((c_lower_pos = t_lower_pos) AND (c_middle_pos = t_middle_pos) AND (c_upper_pos = t_upper_pos)) then
+			moving <= '0';
+		else 
+			moving <= '1';
+		end if;
+
+		l1 <= l_pwm;
+		l2 <= l_pwm;
+		m1 <= m_pwm;
+		m2 <= m_pwm;
+		u1 <= u_pwm;
+		u2 <= u_pwm;
+	end process;
+end v1;
+
+
---
+++ Hardware/rra_controller.vhd
@@ -0,0 +1,113 @@
+library ieee;
+use IEEE.std_logic_1164.all;
+
+entity controller is
+port (
+        clk       : in  std_ulogic;
+        rst       : in  std_ulogic;
+        
+        mode      : in  std_ulogic_vector(1 downto 0);
+        moving    : in  std_ulogic;
+        store     : in  std_ulogic;
+        l_keypad  : out std_ulogic;
+        l_memory  : out std_ulogic;
+        w_memory  : out std_ulogic
+);
+end controller;
+
+architecture v1 of controller is
+
+  type STATE is (s_WAITING, s_LOAD_KEYPAD, s_CHECK_KEYPAD, 
+                 s_SET_MEMORY_NEXT, s_GET_MEMORY_NEXT,
+                 s_MOVING);
+  signal c_state, n_state: STATE;
+
+begin
+
+  process (clk, rst)
+  begin
+    if (rst = '1') then
+      c_state <= s_WAITING;
+  elsif (rising_edge(clk)) then
+      c_state <= n_state;
+  end if;
+  end process;
+
+  process (c_state)
+  begin
+    case c_state is
+      when s_WAITING =>
+        case mode is
+          when "00" =>  --Manual
+            l_keypad <= '1';
+            l_memory <= '0';
+            n_state  <= s_LOAD_KEYPAD;
+          when "01" =>  --Record
+            l_keypad <= '0';
+            l_memory <= '0';
+            n_state  <= s_CHECK_KEYPAD;
+          when "10" =>  --Playback
+            l_keypad <= '0';
+            l_memory <= '1';
+            n_state  <= s_GET_MEMORY_NEXT;
+          when others=> --Unknown
+            l_keypad <= '0';
+            l_memory <= '0';
+            n_state  <= s_WAITING;
+        end case;
+
+        w_memory <= '0';
+
+      when s_LOAD_KEYPAD =>
+        l_keypad <= '0';
+        l_memory <= '0';
+        w_memory <= '0';
+        if moving = '1' then
+          n_state <= s_MOVING;
+        else 
+          n_state <= s_WAITING;
+        end if;
+
+      when s_CHECK_KEYPAD =>
+        if store = '1' then
+          l_keypad <= '0';
+          w_memory <= '1';
+          n_state  <= s_SET_MEMORY_NEXT;
+        else 
+          l_keypad <= '1';
+          w_memory <= '0';
+          n_state  <= s_LOAD_KEYPAD;
+        end if;
+
+        l_memory <= '0';
+
+      when s_GET_MEMORY_NEXT =>
+        l_keypad <= '0';
+        l_memory <= '0';
+        w_memory <= '0';
+        if moving = '1' then
+          n_state <= s_MOVING;
+        else 
+          n_state <= s_WAITING;
+        end if;
+
+      when s_SET_MEMORY_NEXT =>
+        l_keypad <= '0';
+        l_memory <= '0';
+        w_memory <= '0';
+        n_state <= s_WAITING;
+
+      when s_MOVING =>
+        l_keypad <= '0';
+        l_memory <= '0';
+        w_memory <= '0';
+        if moving = '1' then
+          n_state <= s_MOVING;
+        else 
+          n_state <= s_WAITING;
+        end if;
+    end case;
+
+  end process;
+
+end v1;
